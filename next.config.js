acef135b6197e002001cc0980b2f099f23a094e0
Ruru Huang <huangjulu123@gmail.com>
2024-07-08 12:11:10 +0800

    change img path

--- next.config.js
+++ next.config.js
@@ -3,12 +3,13 @@
 const isProd = process.env.NODE_ENV === 'production'; // npm run build
 
 const nextConfig = {
+    assetPrefix: isProd ? '/your-repository-name/' : '',
     basePath: isProd ? '/next-test' : '',
     output:'export',
     distDir:'dist',
     images: {
-        loader: 'akamai',
-        path: '',
+        loader: 'default',
+        path: '/',
       },
 };
 
